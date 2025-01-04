058d879a950ff5a70475eea68ff760b7cfc8c5d3
naitikmundra <naitikmundra18@gmail.com>
2025-01-04 12:08:28 +0530

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-
+//THIS IS THE EXTENSION'S BACKEND CODE
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
@@ -6,32 +6,26 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-// When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
 
-    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
     const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
-    // Set the action badge to the next state
     await chrome.action.setBadgeText({
       tabId: tab.id,
       text: nextState
     });
 
     if (nextState === 'ON') {
-      // Insert the CSS file when the user turns the extension on
       await chrome.scripting.insertCSS({
         files: ['focus-mode.css'],
         target: { tabId: tab.id }
       });
         await chrome.scripting.executeScript({
-          files: ['content.js'], // Replace with your JavaScript file name
+          files: ['content.js'], 
           target: { tabId: tab.id }
         });
     } else if (nextState === 'OFF') {
-      // Remove the CSS file when the user turns the extension off
       await chrome.scripting.removeCSS({
         files: ['focus-mode.css'],
         target: { tabId: tab.id }
